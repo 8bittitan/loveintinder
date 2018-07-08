f5347d2bbeb126f49fbca8204b687aa8e8fb1d60
Paul Jankowski <paul.jankowski@dominos.com>
2018-07-08 00:08:57 -0400

    Convert all svgs to inline

--- src/components/Episode/Episode.js
+++ src/components/Episode/Episode.js
@@ -1,23 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import ListPlay from '../../images/list-play.svg'
-import MiniEqualizer from '../../images/equalizer-mini.svg'
-
 import './Episode.styl'
 
 const Episode = ({ episode }) => (
   <div className="Episode">
-    <img
-      src={ListPlay}
-      alt={`Play ${episode.title}`}
+    <svg
+      width="27"
+      height="27"
+      viewBox="0 0 27 27"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
       className="Episode__Play"
-    />
+    >
+      <path
+        d="M9 0L16.7942 13.5H1.20577L9 0Z"
+        transform="translate(22 4) rotate(90)"
+        fill="#C5C6B7"
+      />
+      <circle cx="13.5" cy="13.5" r="13" stroke="#C5C6B7" />
+    </svg>
     <div className="Episode__Information">
       <h3 className="Episode__Information__Title">{episode.title}</h3>
-      <p className="Episode__Information__Description">{episode.contentSnippet}</p>
+      <p className="Episode__Information__Description">
+        {episode.contentSnippet}
+      </p>
     </div>
-    <img src={MiniEqualizer} className="Episode__EQ" />
+    <svg
+      width="25"
+      height="14"
+      viewBox="0 0 25 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="Episode__EQ"
+    >
+      <rect width="5" height="13" transform="translate(0 1)" fill="#C4C4C4" />
+      <rect width="5" height="7" transform="translate(5 7)" fill="#C4C4C4" />
+      <rect width="5" height="7" transform="translate(20 7)" fill="#C4C4C4" />
+      <rect width="5" height="10" transform="translate(10 4)" fill="#C4C4C4" />
+      <rect width="5" height="14" transform="translate(15)" fill="#C4C4C4" />
+    </svg>
   </div>
 )
 
